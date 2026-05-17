7e7a9dcf6cce35ce5019c3b647d3654f9d316614
Ikeda Harket <sebaaignacio111@gmail.com>
2026-05-17 02:16:42 -0400

    rebrand business

---
+++ tailwind.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "class", // <--- Activa la estrategia por clase
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
