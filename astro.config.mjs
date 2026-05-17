5ee1306192b5aebd09af0f10672bf835469f9a40
Ikeda Harket <sebaaignacio111@gmail.com>
2026-05-17 19:48:09 -0400

    planes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@
 import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [
+    icon(), // 👈 2. La registramos aquí para levantar el módulo virtual 'virtual:astro-icon'
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
