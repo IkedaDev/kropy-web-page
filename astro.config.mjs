dfadfdcc9db82ade1c08be42d64e436f4657b6fe
Ikeda Harket <sebaaignacio111@gmail.com>
2026-05-18 15:39:17 -0400

    tailwind desacoplado

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 
 // https://astro.build/config
@@ -9,7 +8,4 @@ export default defineConfig({
   integrations: [
     icon(), // 👈 2. La registramos aquí para levantar el módulo virtual 'virtual:astro-icon'
   ],
-  vite: {
-    plugins: [tailwindcss()],
-  },
 });
